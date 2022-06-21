31dd5f12491f2c24faa7ca102c8dc0abe54eebca
Uncontained0 <uncontained0@gmail.com>
2022-06-20 19:36:18 -0500

    init

---
+++ src/init.lua
@@ -0,0 +1,69 @@
+local Packages = script.Parent
+
+local Object = require(Packages.Object)
+
+local Pipe = Object:Extend()
+
+function Pipe:Constructor()
+	self._CallbackList = {}
+	self._HighestPriority = 0
+end
+
+function Pipe:Listen(Callback: (next: (...any) -> (), ...any) -> (), Priority: number?)
+	Priority = Priority or 1
+
+	if self._HighestPriority < Priority then
+		self._HighestPriority = Priority
+	end
+
+	if self._CallbackList[Priority] == nil then
+		self._CallbackList[Priority] = {}
+	end
+
+	self._CallbackList[Priority][#self._CallbackList[Priority] + 1] = Callback
+end
+
+function Pipe:Send(...)
+	local Args = { ... }
+	local Continue = false
+
+	local i = 0
+	while i < self._HighestPriority do
+		i += 1
+
+		for _, v in ipairs(self._CallbackList[i] or {}) do
+			Continue = false
+
+			local Value = { v(function(...)
+				Continue = true
+				return ...
+			end, unpack(Args)) }
+
+			if #Value ~= 0 then
+				Args = Value
+			end
+
+			if Continue == false then
+				return unpack(Args)
+			end
+		end
+	end
+
+	return unpack(Args)
+end
+
+function Pipe:Destroy()
+	self._CallbackList = nil
+	self._HighestPriority = nil
+end
+
+export type Pipe = Object.Object<{
+	Listen: (self: Pipe, (next: (...any) -> (), ...any) -> (), Priority: number) -> (),
+	Send: (...any) -> (...any),
+	Destroy: () -> (),
+
+	_CallbackList: { (next: (...any) -> (), ...any) -> () },
+	_HighestPriority: number,
+}>
+
+return Pipe :: Pipe
---
+++ src/pipe.spec.lua
@@ -0,0 +1,82 @@
+--# selene: allow(undefined_variable)
+
+return function()
+	local Pipe = require(script.Parent)
+
+	describe("pipe", function()
+		
+		it("should send", function()
+			local p = Pipe:New()
+
+			local value = false
+
+			p:Listen(function()
+				value = true
+			end)
+
+			p:Send()
+
+			expect(value).to.equal(true)
+		end)
+
+		it("should continue and mutate data", function()
+			local p = Pipe:New()
+
+			p:Listen(function(next, v)
+				return next(v + 5)
+			end)
+
+			p:Listen(function(next, v)
+				return next(v + 10)
+			end)
+
+			local value = p:Send(0)
+
+			expect(value).to.equal(15)
+		end)
+
+		it("should follow priority", function()
+			local p = Pipe:New()
+
+			local t1, t2
+
+			p:Listen(function(next)
+				t1 = tick()
+				task.wait()
+				next()
+			end, 2)
+
+			p:Listen(function(next)
+				t2 = tick()
+				task.wait()
+				next()
+			end, 1)
+
+			p:Send()
+
+			expect(t1 > t2).to.equal(true)
+		end)
+
+		it("should allow non-consecutive priority", function()
+			local p = Pipe:New()
+
+			local t1, t2
+
+			p:Listen(function(next)
+				t1 = tick()
+				task.wait()
+				next()
+			end, 5)
+
+			p:Listen(function(next)
+				t2 = tick()
+				task.wait()
+				next()
+			end, 2)
+
+			p:Send()
+
+			expect(t1 > t2).to.equal(true)
+		end)
+	end)
+end
